01519ac176f30a1bdf3cbf74ff863b2fc96ae450
Chima Precious <ositachima.co@gmail.com>
2019-09-26 21:36:38 +0000

    Refactor controllers

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,3 +1,31 @@
 const wooClient = require('../config/woocommerce');
 
 exports.getProducts = async (req, res) => wooClient.getProducts(req.query).then((result) => res.send(result.data)).catch((error) => res.status(422).send(error.response.data));
+
+exports.getProduct = async (req, res) => {
+    var productId = req.params.productId;
+
+
+    if (!parseInt(productId)) return res.status(422).json({
+        message: 'Product Id must be a number',
+        status: 422
+    });
+
+
+    try {
+
+        const result = await wooClient.getProduct(productId);
+
+        const reviews = await wooClient.getReviews(
+            { product: `${productId}` }
+        );
+
+        result.data['reviews'] = reviews.data;
+        res.send(result.data);
+
+    } catch (error) {
+        res.status(500).json({
+            message: error
+        })
+    }
+}
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,20 +1,25 @@
-const { validationResult } = require('express-validator');
 const wooClient = require('../config/woocommerce');
 const request = require('request');
 
-exports.createUser = async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty())
-        return res.status(422).json({ errors: errors.array() });
+exports.createUser = async (req, res) => wooClient.createCustomer(req.body).then((result) => res.send(result.data)).catch((error) => res.status(422).send(error.response.data));
 
-    return wooClient.createCustomer(req.body).then((result) => res.send(result.data)).catch((error) => res.status(422).send(error.response.data));
-};
+exports.loginUser = async (req, res) => request.post({
+    headers: { 'content-type': 'application/x-www-form-urlencoded' },
+    url: `${process.env.CONSUMER_WEBSITE}/wp-json/jwt-auth/v1/token`,
+    form: {
+        username: req.body.username,
+        password: req.body.password
+    }
+}, function (error, response, body) {
 
-exports.loginUser = async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty())
-        return res.status(422).json({ errors: errors.array() });
-}
+    if (error) return res.status(response.statusCode).json({
+        message: error,
+    });
+
+    res.set('Content-Type', 'application/json; charset=UTF-8').send(body);
+});
+
+
 
 exports.getMyAccount = async (req, res) => request.post({
     url: `${process.env.CONSUMER_WEBSITE}/wp-json/wp/v2/users/me`,
